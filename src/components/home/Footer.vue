615fab5f36a2f721b4a1502311f45f39221ec64f
Gustavo Kley <gustavokley08@gmail.com>
2025-07-09 15:21:03 -0300

    Criando o footer no meu ecommerce

---
+++ src/components/home/Footer.vue
@@ -0,0 +1,48 @@
+<template>
+<footer class="bg-dark text-light py-5">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-3">
+          <h4>GGTECH</h4>
+          <p>Sua loja especializada em hardware gamer e periféricos de alta performance.</p>
+          <div class="d-flex social-icons">
+            <a href="#" class="text-light me-3"><i class="fab fa-facebook-f"></i></a>
+            <a href="#" class="text-light me-3"><i class="fab fa-instagram"></i></a>
+            <a href="#" class="text-light me-3"><i class="fab fa-twitter"></i></a>
+            <a href="#" class="text-light"><i class="fab fa-youtube"></i></a>
+          </div>
+        </div>
+        <div class="col-md-3">
+          <h5>Links Rápidos</h5>
+          <ul class="list-unstyled">
+            <li><a href="#" class="text-light text-decoration-none">Sobre Nós</a></li>
+            <li><a href="#" class="text-light text-decoration-none">Produtos</a></li>
+            <li><a href="#" class="text-light text-decoration-none">Ofertas</a></li>
+            <li><a href="#" class="text-light text-decoration-none">Blog</a></li>
+          </ul>
+        </div>
+        <div class="col-md-3">
+          <h5>Suporte</h5>
+          <ul class="list-unstyled">
+            <li><a href="#" class="text-light text-decoration-none">Central de Ajuda</a></li>
+            <li><a href="#" class="text-light text-decoration-none">Política de Troca</a></li>
+            <li><a href="#" class="text-light text-decoration-none">Garantia</a></li>
+            <li><a href="#" class="text-light text-decoration-none">Contato</a></li>
+          </ul>
+        </div>
+        <div class="col-md-3">
+          <h5>Contato</h5>
+          <ul class="list-unstyled">
+            <li><a href="mailto:contato@gametech.com" class="text-light text-decoration-none"><i class="fas fa-envelope me-2"></i>contato@ggtech.com</a></li>
+            <li><a href="tel:+5511999999999" class="text-light text-decoration-none"><i class="fas fa-phone me-2"></i>(42) 9999-9999</a></li>
+            <li><a href="#" class="text-light text-decoration-none"><i class="fas fa-map-marker-alt me-2"></i>Guarapuava, PR</a></li>
+          </ul>
+        </div>
+      </div>
+      <hr class="mt-4 border-secondary">
+      <div class="text-center mt-3">
+        <p class="mb-0">&copy; 2025 GGTECH. Todos os direitos reservados.</p>
+      </div>
+    </div>
+  </footer>
+  </template>
